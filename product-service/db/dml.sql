ab6d1d05bec8a73148214f829b8ca655cd001979
Pavel Fralou <pavel_fralou@epam.com>
2021-09-06 17:38:45 +0300

    feat: change products

--- product-service/db/dml.sql
+++ product-service/db/dml.sql
@@ -1,25 +1,25 @@
 insert into product (title, description, price)
-values ('Aws ProductOne', 'Aws Short Product Description1', 2.4),
-  ('Aws ProductNew', 'Aws Short Product Description3', 10),
-  ('Aws ProductTop', 'Aws Short Product Description2', 23),
-  ('ProductTitle', 'Aws Short Product Description7', 15),
-  ('Aws Product', 'Aws Short Product Description2', 23),
-  ('Aws ProductTest', 'Aws Short Product Description4', 15),
-  ('Aws Product2', 'Aws Short Product Descriptio1', 23),
-  ('Aws ProductName', 'Aws Short Product Description7', 15)
+values ('Aws PG Product-1', 'Aws PG Short Product Description-1', 2.4),
+  ('Aws PG Product-2', 'Aws PG Short Product Description-2', 10),
+  ('Aws PG Product-3', 'Aws PG Short Product Description-3', 23),
+  ('Aws PG Product-4', 'Aws PG Short Product Description-4', 15),
+  ('Aws PG Product-5', 'Aws PG Short Product Description-5', 23),
+  ('Aws PG Product-6', 'Aws PG Short Product Description-6', 15),
+  ('Aws PG Product-7', 'Aws PG Short Product Description-7', 23),
+  ('Aws PG Product-8', 'Aws PG Short Product Description-8', 15)
 on conflict (title) do update
 set description = EXCLUDED.description,
 	price = EXCLUDED.price;
 
 insert into stock (product_id, count)
-values ((select id from product where title = 'Aws ProductOne'), 4),
-	((select id from product where title = 'Aws ProductNew'), 6),
-  ((select id from product where title = 'Aws ProductTop'), 7),
-  ((select id from product where title = 'ProductTitle'), 12),
-  ((select id from product where title = 'Aws Product'), 7),
-  ((select id from product where title = 'Aws ProductTest'), 8),
-  ((select id from product where title = 'Aws Product2'), 2),
-  ((select id from product where title = 'Aws ProductName'), 3)
+values ((select id from product where title = 'Aws PG Product-1'), 4),
+	((select id from product where title = 'Aws PG Product-2'), 6),
+  ((select id from product where title = 'Aws PG Product-3'), 7),
+  ((select id from product where title = 'Aws PG Product-4'), 12),
+  ((select id from product where title = 'Aws PG Product-5'), 7),
+  ((select id from product where title = 'Aws PG Product-6'), 8),
+  ((select id from product where title = 'Aws PG Product-7'), 2),
+  ((select id from product where title = 'Aws PG Product-8'), 3)
 on conflict (product_id) do update
 set count = EXCLUDED.count;
 
